89b3249a7d7429546fd0c6e28d9e7010f707f7cc
Joe <rezenbiz@gmail.com>
2017-10-23 23:48:05 -0700

    added cipher

--- tools/app.js
+++ tools/app.js
@@ -46,3 +46,33 @@ A B C D E F G H I J K L M
 N O P Q R S T U V W X Y Z
 
 */
+
+
+const cipher = {
+  A: 1,
+  B: 2,
+  C: 3,
+  D: 4,
+  E: 5,
+  F: 6,
+  G: 7,
+  H: 6,
+  I: 5,
+  J: 4,
+  K: 3,
+  L: 2,
+  M: 1,
+  N: 1,
+  O: 2,
+  P: 3,
+  Q: 4,
+  R: 5,
+  S: 6,
+  T: 7,
+  U: 6,
+  V: 5,
+  W: 4,
+  X: 3,
+  Y: 2,
+  Z: 1
+}
